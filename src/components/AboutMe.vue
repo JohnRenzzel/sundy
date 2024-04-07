7fb57ec45dd92c00ee37b27d0c5f34ac95fc03eb
John Renzzel <ronztajanlangit09@gmail.com>
2024-04-08 01:46:46 +0800

    Initial commit

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="max-w-7xl m-center pt-20 pb-20">
+    <div
+      class="about-me text-center mt-10 md:mt-32"
+      data-aos="fade-up"
+      data-aos-delay="100"
+      data-aos-duration="800"
+    >
+      <h2 class="text-5xl dark:text-white">About Me</h2>
+    </div>
+    <div
+      class="details flex mt-20 md:flex-col-reverse lg:flex-row flex-col-reverse"
+    >
+      <div
+        class="image lg:w-3/6 mt-12 lg:m-auto"
+        data-aos="fade-right"
+        data-aos-delay="500"
+        data-aos-duration="500"
+      >
+        <img
+          class="w-11/12 rounded-md drop-shadow-lg m-auto lg:m-0"
+          src="../assets/img/self/self1.jpg"
+          alt=""
+        />
+      </div>
+      <div
+        class="information lg:w-3/6 flex flex-col"
+        data-aos="fade-right"
+        data-aos-delay="1200"
+        data-aos-duration="500"
+      >
+        <div
+          class="cover-letter f-gray text-lg pb-7 dark:text-white text-center lg:text-left md:justify-center lg:justify-start"
+        >
+          Alongside my academic journey, I've actively pursued opportunities to
+          expand my skill set through hands-on training. My time at TESDA CCA
+          training, where I successfully finished a Creative Web Design course,
+          underscores my commitment to continuous growth and learning.
+          Currently, I'm engaged in advancing my expertise with Computer System
+          Servicing NC II. Moreover, my academic track record reflects a history
+          of excellence—from receiving honors consistently since elementary
+          school up to grade 11. Beyond academics, I embody a strong work ethic
+          and possess robust leadership abilities. I thrive on motivating and
+          guiding teams towards realizing their objectives. Remember, each step
+          forward in your education and training is a testament to your
+          dedication and determination. Keep pushing your boundaries and
+          embracing new challenges—it's through these experiences that we truly
+          evolve and excel.
+        </div>
+        <div
+          class="cover-letter f-gray text-lg pb-7 dark:text-white text-center lg:text-left md:justify-center lg:justify-start"
+        >
+          <div class="personal-info mr-5 w-full lg:w-auto">
+            <p><strong>Name:</strong> Sundy T. Diez</p>
+            <p><strong>Age:</strong> 25</p>
+            <p><strong>Address:</strong> Gabi, Cordova, Cebu</p>
+            <p><strong>Gender:</strong> Male</p>
+            <p><strong>Birthdate:</strong> July 26, 1998</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
